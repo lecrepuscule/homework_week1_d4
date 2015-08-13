fd123cb6f85ffbbd207331da6f4b8b195fb4eb60
Yao Yuan <opal_obsidian@hotmail.com>
2015-08-13 17:45:15 +0100

    event listener to show hidden element for basic calc

---
+++ advanced_calc/js/main.js
@@ -0,0 +1,16 @@
+
+// function runCalculator(buttonId, answerId) {
+//   getInput();
+//   doMath();
+//   showAnswer();
+// }
+
+
+var basicAnswer = document.getElementById("basic-answer");
+
+var basicButton = document.getElementById("basic-calc");
+
+basicButton.addEventListener("click", function() {basicAnswer.className = "show"});
+
+
+
