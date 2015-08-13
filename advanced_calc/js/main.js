5f38b29775d0fd4d78dcaaa3702d25bca82c0d6f
Yao Yuan <opal_obsidian@hotmail.com>
2015-08-13 18:07:29 +0100

    can get input values

--- advanced_calc/js/main.js
+++ advanced_calc/js/main.js
@@ -10,7 +10,22 @@ var basicAnswer = document.getElementById("basic-answer");
 
 var basicButton = document.getElementById("basic-calc");
 
-basicButton.addEventListener("click", function() {basicAnswer.className = "show"});
+var basicInput1 = document.getElementById("basic-num-1");
+
+var basicInput2 = document.getElementById("basic-num-2");
+
+function getInput() {
+  return [basicInput1.value, basicInput2.value];
+}
+
+var inputs = getInput();
+
+console.log(inputs[0] + inputs[1]);
+
+basicButton.addEventListener("click", function() {
+  var inputs = getInput();
+  console.log(inputs[0] + inputs[1]); basicAnswer.className = "show";
+})
 
 
 
